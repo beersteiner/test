b83cc2457e22b41a1db3dbed9be66fcdd26a6f8c
John <johndstein@hotmail.com>
2018-02-02 09:31:45 -0500

    load.py working

--- load.py
+++ load.py
@@ -1,86 +1,92 @@
-import mysql.connector
-import numpy as np
-import pandas as pd
-import csv
+import pymysql
+import sqlalchemy as sa
+from odo import odo
 
-## GLOBALS
+
+## CLASS DEFINITIONS
+
+# DO NOT MODIFY! - Table structure information object
+class tabEntry:
+    def __init__(self, name=None, csv=None, cols=None):
+        self.name=name # name of table
+        self.csv=csv   # path to csv source (string)
+        self.cols=cols # list of tuples [(cName, cType), ...]
+
+
+## GLOBALS - Modify!
 
 wCliOpt = '/home/john/client-ssl/CastawayCay_MySQL/.writeclient.cnf'
-rCliOpt = '/home/john/client-ssl/CastawayCay_MySQL/.readclient.cnf'
 DB_NAME = 'iris'
-D_FILE = 'iris_ch.data'
-CNK_SIZE = 100
-
-# Table Creation Statements
-TABLES = {}
-TABLES['data'] = {}
-TABLES['data'] = {
-        'csv':'iris_ch.data',
-        'create':(
-                "CREATE TABLE `data` ("
-                "    `sepal-length` FLOAT(10),"
-                "    `sepal-width` FLOAT(10),"
-                "    `petal-length` FLOAT(10),"
-                "    `petal-width` FLOAT(10),"
-                "    `class` VARCHAR(20)"
-                ")"),
-        'populate':(
-                "INSERT INTO data "
-                "(sepal-length, sepal-width, petal-length, petal-width, class) "
-                "VALUES (%f, %f, %f, %f, %s)")
-        }
+TABLES = []
+TABLES.append(tabEntry(name = 'data',
+                       csv = 'iris_ch.data',
+                       cols = [('sepal_length', sa.Float),
+                               ('sepal_width', sa.Float),
+                               ('petal_length', sa.Float),
+                               ('petal_width', sa.Float),
+                               ('class', sa.String(20))]))
 
 
 ## FUNCTIONS
 
-# DO NOT MODIFY!
-# Creates a database or returns an error
+# DO NOT MODIFY! - Creates a connection to MySQL Server
+def mysql_connect_w():
+    return pymysql.connect(read_default_file=wCliOpt)
+
+# DO NOT MODIFY! - Creates a connection to MySQL Database
+def db_connect_w():
+    return pymysql.connect(read_default_file=wCliOpt, db=DB_NAME)
+
+# DO NOT MODIFY! - Creates a database or returns an error
 def create_database(cursor):
+    print 'Database `{}`:'.format(DB_NAME),
     try: cursor.execute(
             'CREATE DATABASE {}'.format(DB_NAME))
-    except mysql.connector.Error as err:
-        print('Failed creating database: {}'.format(err))
-        exit(1)
+    except pymysql.MySQLError as err:
+        print 'already exists.'
+    else:
+        print 'OK.'
+
+# DO NOT MODIFY! - Adds an additional auto-increment column to table
+def add_autoinc_col(db, t):
+    conn = db.raw_connection()
+    cursor = conn.cursor()
+    cursor.execute('ALTER TABLE `'+t+'` ADD `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST')
+    cursor.close() # Close handle for SQL server
+    conn.close() # Close the connection to the SQL server
 
 
 ## MAIN
 
-# DO NOT MODIFY!
-# Establish connection and cursor
-conn = mysql.connector.connect(option_files=wCliOpt) # establish connection
-cursor = conn.cursor()                               # grab a cursor
-# Use or establish database
-try: conn.database = DB_NAME # try to use database
-except mysql.connector.Error as err:
-    if err.errno == mysql.connector.errorcode.ER_BAD_DB_ERROR:
-        create_database(cursor) # if database doesn't exist, create it
-        conn.database = DB_NAME # use the created database
-    else: # print any other error
-        print(err)
-        exit(1)
-# Create tables
-for table in TABLES.keys(): # Iterate through the table creation statements
-    try:
-        print 'Creating table %s: ' % table,
-        cursor.execute(TABLES[table]['create']) # create the table
-    except mysql.connector.Error as err:
-        if err.errno == mysql.connector.errorcode.ER_TABLE_EXISTS_ERROR:
-            print 'already exists.'
-        else:
-            print err.msg
-    else:
-        # Populate the table
-        data = csv.reader(file(TABLES[table]['csv']))
-        next(data) # skip header file
-        for row in data:
-            print row
-            cursor.execute(TABLES[table]['populate'], row)
-        print 'OK.'
-
-
-
-
-
-
+# Create Database
+sql_eng = sa.create_engine('mysql+pymysql://', creator=mysql_connect_w)
+conn = sql_eng.raw_connection()
+cursor = conn.cursor() 
+create_database(cursor)
 cursor.close() # Close handle for SQL server
 conn.close() # Close the connection to the SQL server
+
+# Creat the sqlalchemy engine
+db_eng = sa.create_engine('mysql+pymysql://', creator=db_connect_w)
+metadata = sa.MetaData(bind=db_eng) # holds database information
+
+# Loop through all table entries
+for te in TABLES:
+    # create the sqlalchemy.table object
+    tbl = sa.Table(
+        te.name,
+        metadata,
+        *[sa.Column(c[0],c[1]) for c in te.cols])
+    print 'Table `{}`:'.format(te.name),
+    if tbl.exists(): 
+        print 'exists; over-writing.',
+        tbl.drop(checkfirst=True)
+    tbl.create()
+    add_autoinc_col(db_eng, te.name)    # Add AUTOINCREMENT COLUMN
+    odo(te.csv, tbl)                    # bulk-load the .csv using odo
+    print 'OK.'
+print 'Table loading complete!'
+
+
+
+
